72ff5160cfb947106e1fa36d1ef4fb93bdc7776a
Echoandland <e1297769@u.nus.edu>
2026-01-22 23:58:37 +0800

    Level-3: mark and unmark tasks

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -12,27 +12,47 @@ public class ChatBot {
         System.out.println("What can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         
         while (true) {
+
             String input = scanner.nextLine().trim();
+
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
+
+            if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5).trim()) - 1;
+                tasks.get(index).markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks.get(index));
+                continue;
+            }
+
+            if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7).trim()) - 1;
+                tasks.get(index).unmarkDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks.get(index));
+                continue;
+            }
+
             if (input.equalsIgnoreCase("list")) {
                 if (tasks.isEmpty()) {
                     System.out.println("No tasks yet.");
                 } else {
                     for (int i = 0; i < tasks.size(); i++) {
                         System.out.println((i + 1) + ". " + tasks.get(i));
+
                     }
                 }
                 continue;
             }
-            tasks.add(input);
+            tasks.add(new Task(input));
             System.out.println("Added: " + input);
-            
+
             if (input.trim().equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return (isDone ? "[X] " : "[ ] ") + description;
+    }
+}
